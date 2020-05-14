16b50280a554e7eca5be3b9c2a75858a2a8f7793
MantelosSpyros <mantelospyros@gmail.com>
2020-05-14 23:09:57 +0300

    heuristic

--- Test/Heuristic.java
+++ Test/Heuristic.java
@@ -4,88 +4,45 @@ import java.io.IOException;
 
 public class Heuristic
 {
-    public int heuristic(String permutation)
+    public int heuristic(String permutation, int whiteBalls)
     {
-        ArrayList<Integer> left = new ArrayList<>();
-
-        ArrayList<Integer> right = new ArrayList<>();
-
-        //Find center position
-        int center = permutation.length()/2;
-
-        //Find the index of '-'
-        int blankPosition = permutation.indexOf('-');
-
-
-        for(int i = 0; i < center; i++)
-        {
-            //Find positions < n+1 that contain defective 'A'
-            if(permutation.charAt(center - i - 1) == 'A')
-            {
-                left.add(center - i - 1);
+        int extraValue = 0;
+        int emptyIndex = permutation.indexOf('-');
+        int[] left = new  int[whiteBalls];
+        int leftCount = 0;
+        int[] right = new int[whiteBalls];
+        int rightCount = 0;
+        // Go through the whole string
+        for (int i = 0 ; i < 2*whiteBalls+1 ; i++){
+            if ( i < whiteBalls && permutation.charAt(i) == 'A')
+                left[leftCount++] = i;
+            else if (i >= whiteBalls && permutation.charAt(i) == 'M')
+                right[rightCount++] = i;
+        }
+        int full_counter = leftCount + rightCount; //Number of letters in wrong positions
+        int new_right = 0;
+        while(full_counter>0){
+            if (emptyIndex >= whiteBalls){
+                extraValue +=  (emptyIndex - left[--leftCount] );
+                emptyIndex = left[leftCount];
+            }else{
+                extraValue += (right[new_right] - emptyIndex);
+                emptyIndex = right[new_right++];
             }
-
-            //Find positions > n+1 that contain defective 'M'
-            if(permutation.charAt(center + i + 1) == 'M')
-            {
-                right.add(center + i + 1);
-            }
+            full_counter--;
         }
-
-        //Merge the arrays by alternating left and right ArrayList elements
-        ArrayList<Integer> merged = new ArrayList<>();
-
-        if(blankPosition < center)
-        {
-            merged.add(blankPosition);
-            merged.addAll(merge(right, left));
-        }
-        else
-        {
-            merged.add(blankPosition);
-            merged.addAll(merge(left, right));
-        }
-
-        int score = 0;
-
-        //Find score by calculating the sum of the absolute value of the difference
-        // per two elements of the merged ArrayList
-        for(int i = 0; i < merged.size() - 1; i++)
-        {
-            score += Math.abs(merged.get(i) - merged.get(i + 1));
-        }
-
-        return score;
+        return extraValue;
     }
 
-    //Merging of two arrays routine
-    private ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b)
-    {
-        ArrayList<Integer> merged = new ArrayList<>();
-        while(!a.isEmpty() && !b.isEmpty())
-        {
-            merged.add(a.remove(0));
-            merged.add(b.remove(0));
-        }
-        if(!a.isEmpty())
-        {
-            merged.addAll(a);
-        }
-        else if(!b.isEmpty())
-        {
-            merged.addAll(b);
-        }
-
-        return merged;
-    }
 
     //Driver main
     public static void main(String[] args)
     {
         String s = args[0];
+        int whiteBalls = s.length()/2;
         Heuristic h = new Heuristic();
         try {
-            Files.write(Paths.get("heur.txt"), (String.valueOf(h.heuristic(s))+"\n").getBytes(), StandardOpenOption.APPEND);
+            Files.write(Paths.get("heur.txt"), (String.valueOf(h.heuristic(s,whiteBalls))+"\n").getBytes(), StandardOpenOption.APPEND);
         }catch (IOException e) {
             //exception handling left as an exercise for the reader
         }
